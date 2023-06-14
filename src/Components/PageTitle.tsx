f42095c3b4373968b8dd98a284f998f268c7360e
Your Name <kostyaraf@gmail.com>
2023-06-15 02:46:17 +0300

    Fixed title page

--- src/Components/PageTitle.tsx
+++ src/Components/PageTitle.tsx
@@ -4,5 +4,5 @@ type Props = {
   children?: ReactNode;
 };
 export default function PageTitle({ children }: Props) {
-  return <h1 style={{marginBottom: 20, fontWeight: 'bold', fontSize: 20}}>{children}</h1>;
+  return <h1 style={{marginBottom: 20, fontWeight: 'bold', fontSize: 40}}>{children}</h1>;
 }
