004a43d34ed79722b32a9da2a03a85d1ac7424d6
Your Name <kostyaraf@gmail.com>
2023-06-15 02:35:38 +0300

    Fixed home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,32 @@
-import { revalidatePath } from "next/cache";
+import PageTitle from "@/Components/PageTitle";
 import Link from "next/link";
 
-const todos: string[] = ["First goal"];
-
 export default async function Home() {
-  "use server"
-  async function addToDo (data: FormData) {
-    "use server"
-    const todo = data.get('todo') as string
-    todos.push(todo);
-    revalidatePath('/');
-  }
-
   return (
     <main className="p-5">
-      <h1 className="text-4xl font-bold">Todo list:</h1>
+      <PageTitle>Home</PageTitle>
       <ul>
-        <li>
-          <Link href="/todo-list">Todo List</Link>
+        <li className="mb-2">
+          <Link href="/todo-list" className="text-1xl">
+            Todo List
+          </Link>
+          <div className="text-gray-500">
+            List saving in server component state <br />
+            (yeah, not recommended, but interesting)
+          </div>
+        </li>
+        <li className="mb-2">
+          <Link href="/products" className="text-1xl">
+            Products
+          </Link>
+          <div className="text-gray-500">
+            Mix server & client components.
+            <br />
+            Server component get fetch request and saving in
+            <b>client component state</b>
+          </div>
         </li>
       </ul>
-      <ul>
-        <li>
-          <Link href="/products">Products</Link>
-        </li>
-      </ul>
-      <form action={addToDo}>
-        <input
-          type="text"
-          name="todo"
-          className="border border-gray-300 rounded-xl py-2 px-2 mr-1"
-        />
-        <button
-          type="submit"
-          className="border disabled:bg-gray-600 hover:bg-blue-600 active:bg-blue-500 bg-blue-700 shadow-lg text-gray-100 rounded-xl py-2 px-2 "
-        >
-          Save
-        </button>
-      </form>
     </main>
   );
 }
